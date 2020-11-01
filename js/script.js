b6652ce12bce4bd4521cd8ae5f0cbfabba086a41
mykereel <mgmartnz@icloud.com>
2020-11-01 16:15:11 -0700

    five day forecast test successful

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ var $cityTemp = $("#temperature");
 var $cityHumid = $("#humidity");
 var $cityWind = $("#wind-speed");
 var $cityUV = $("#uv-index");
+var $cityFive = $('#five-forecast');
 var cityArray = [];
-var cityLat = '';
-var cityLon = '';
+var cityLat = "";
+var cityLon = "";
 var historyEl = $("#history");
 var historyItem = JSON.parse(localStorage.getItem("userInput"));
 // console.log(historyItem);
 
+//Click gets Current Weather, UV Index, and 5-Day Forecast
 $("#search-button").click(function () {
   var userInput = $("#city-input").val();
   // console.log(localStorage.getItem("userInput"));
@@ -31,7 +33,10 @@ function getWeather(city) {
   $("#wind-speed").empty();
   $.ajax({
     type: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=1e3ad008e239358d0ab3741145b4b149",
+    url:
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&APPID=1e3ad008e239358d0ab3741145b4b149",
     success: function (cityJSON) {
       console.log("success", cityJSON);
       $cityName.append("<h2>" + cityJSON.name + " " + m + "</h2>");
@@ -40,9 +45,12 @@ function getWeather(city) {
       $cityWind.append("Wind Speed: ", cityJSON.wind.speed, " MPH");
       var cityLat = cityJSON.coord.lat;
       var cityLon = cityJSON.coord.lon;
+      var cityFive = cityJSON.name;
+      // console.log(cityFive)
       // console.log(cityLat);
       // console.log(cityLon);
       getUV(cityLat, cityLon);
+      getFiveDay(cityFive);
     },
   });
 }
@@ -52,12 +60,33 @@ function getUV(lat, lon) {
   $("#uv-index").empty();
   $.ajax({
     type: "GET",
-    url: "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&APPID=1e3ad008e239358d0ab3741145b4b149",
+    url:
+      "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&APPID=1e3ad008e239358d0ab3741145b4b149",
     success: function (uvJSON) {
       console.log("success", uvJSON);
       $cityUV.append("UV Index: ", uvJSON.value);
     },
   });
+}
+
+// 5-Day Forecast API Call
+function getFiveDay(five) {
+  $("#five-forecast").empty();
+  $.ajax({
+    type: "GET",
+    url:
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      five +
+      "&units=imperial&APPID=1e3ad008e239358d0ab3741145b4b149",
+      success: function (fiveJSON) {
+        console.log("success", fiveJSON);
+        $cityFive.append("Test ", fiveJSON.list);
+      },
+    });
 }
 
 function showHistory() {
