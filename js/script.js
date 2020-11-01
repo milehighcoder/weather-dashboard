7b872232c1c76d01de95eb56f8ecac892c128ae8
mykereel <mgmartnz@icloud.com>
2020-11-01 10:41:22 -0700

    created uv index api call succesfully

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@
 //OpenWeather Endpoint for API Calls = https://api.openweathermap.org
 //UV Index API = http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid=1e3ad008e239358d0ab3741145b4b149
 
-
+var todaysDate = new Date();
+var todaysDateString = todaysDate.toLocaleDateString();
 var $cityName = $("#city-name");
 var $cityTemp = $("#temperature");
 var $cityHumid = $("#humidity");
@@ -24,7 +25,7 @@ function getWeather(city) {
     url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=1e3ad008e239358d0ab3741145b4b149",
     success: function (cityJSON) {
       console.log("success", cityJSON);
-      $cityName.append("<h2>" + cityJSON.name + "</h2>");
+      $cityName.append("<h2>" + cityJSON.name + " " + cityJSON.dt + "</h2>");
       $cityTemp.append("Temperature: ", cityJSON.main.temp, " &deg;F");
       $cityHumid.append("Humidity: ", cityJSON.main.humidity, "%");
       $cityWind.append("Wind Speed: ", cityJSON.wind.speed, " MPH");
@@ -32,10 +33,24 @@ function getWeather(city) {
   });
 }
 
+function getUV() {
+  $("#uv-index").empty();
+  $.ajax({
+    type: "GET",
+    url: "http://api.openweathermap.org/data/2.5/uvi?lat=37.75&lon=-122.37&appid=1e3ad008e239358d0ab3741145b4b149",
+    success: function (uvJSON) {
+      console.log("success", uvJSON);
+      $cityUV.append("UV Index: ", uvJSON.value);
+    },
+  });
+}
+
 function showHistory() {
-  var historyItem = JSON.parse(localStorage.getItem(localStorage.key("userInput")));
+  var historyItem = JSON.parse(
+    localStorage.getItem(localStorage.key("userInput"))
+  );
   historyEl.innerHTML = "";
-  console.log(historyItem)
+  console.log(historyItem);
   if (historyItem !== null) {
     for (i = 0; i < historyItem.length; i++) {
       var searchItem = document.createElement("input");
@@ -54,10 +69,12 @@ function showHistory() {
 showHistory();
 
 function newHistory() {
-  var historyItem = JSON.parse(localStorage.getItem(localStorage.key("userInput")));
+  var historyItem = JSON.parse(
+    localStorage.getItem(localStorage.key("userInput"))
+  );
   if (historyItem !== null) {
     var searchItem = document.createElement("input");
-    console.log(historyItem)
+    console.log(historyItem);
     searchItem.setAttribute("type", " text");
     searchItem.setAttribute("readonly", "true");
     searchItem.setAttribute("class", "form-control d-block bg-white");
@@ -71,9 +88,18 @@ function newHistory() {
 
 $("#search-button").click(function () {
   var userInput = $("#city-input").val();
-  console.log(userInput);
+  console.log(localStorage.getItem("userInput"));
+  if (localStorage.getItem("userInput") !== null) {
+    cityArray = JSON.parse(localStorage.getItem("userInput"));
+  }
   cityArray.push(userInput);
   localStorage.setItem("userInput", JSON.stringify(cityArray));
   getWeather(userInput);
   newHistory();
+  getUV();
 });
+
+// function toLocalDate(unixTime) {
+//   var unixDate = new Date(unixTime * 1000);
+//   return unixDate.toLocaleDateString("en-US");
+// }
